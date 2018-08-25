e3e861f2d96929a4a6d792a019065089c511a502
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-08-25 23:04:41 +0200

    finite caller

---
+++ src/FiniteCaller.php
@@ -0,0 +1,94 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\Recoil;
+
+use Recoil\Kernel;
+use Rx\ObservableInterface;
+use Rx\Subject\Subject;
+
+final class FiniteCaller implements QueueCallerInterface
+{
+    /** @var State */
+    private $state;
+
+    /** @var QueueCallerInterface[] */
+    private $caller = [];
+
+    /** @var State[] */
+    private $callerState = [];
+
+    /** @var Subject */
+    private $callerStream = [];
+
+    /** @var array */
+    private $queue = [];
+
+    /** @var int */
+    private $callersBusy = 0;
+
+    /** @var int */
+    private $callersCount = 0;
+
+    /**
+     * @param Kernel $kernel
+     * @param int    $size
+     */
+    public function __construct(Kernel $kernel, int $size)
+    {
+        $this->callersCount = $size;
+        $this->state = new State();
+        for ($i = 0; $i < $size; $i++) {
+            $subject = new Subject();
+            $caller = new QueueCaller($kernel);
+            $hash = spl_object_hash($caller);
+            $this->caller[$hash] = $caller;
+            $this->callerStream[$hash] = $subject;
+            $this->callerState[$hash] = $caller->call($subject);
+            $this->callerState[$hash]->filter(function () {
+                return count($this->queue) > 0;
+            })->filter(function (int $state) {
+                return $state === State::WAITING;
+            })->subscribe(function () use ($hash) {
+                $this->callerStream[$hash]->onNext(array_shift($this->queue));
+            });
+            $this->callerState[$hash]->subscribe(function () use ($hash) {
+                $this->callersBusy = \count(\array_filter($this->callerState, function (State $state) {
+                    return $state->getState() === State::BUSY;
+                }));
+                if ($this->callersBusy === $this->callersCount) {
+                    $this->state->onNext(State::BUSY);
+
+                    return;
+                }
+
+                $this->state->onNext(State::WAITING);
+            });
+        }
+        $this->state->onNext(State::STARTED);
+    }
+
+    public function call(ObservableInterface $observable): State
+    {
+        $observable->subscribe(function (Call $call) {
+            $this->delegateCall($call);
+        });
+
+        return $this->state;
+    }
+
+    private function delegateCall(Call $call): void
+    {
+        foreach ($this->callerState as $qcHash => $state) {
+            if ($state->getState() !== State::WAITING) {
+                continue;
+            }
+
+            $this->callerStream[$qcHash]->onNext($call);
+
+            return;
+        }
+
+        $this->state->onNext(State::BUSY);
+        $this->queue[] = $call;
+    }
+}
---
+++ tests/FiniteCallerTest.php
@@ -0,0 +1,141 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\Tests\Recoil;
+
+use ApiClients\Tools\TestUtilities\TestCase;
+use React\EventLoop\Factory;
+use React\Promise\Deferred;
+use React\Promise\Promise;
+use Recoil\React\ReactKernel;
+use Rx\Subject\Subject;
+use WyriHaximus\Recoil\Call;
+use WyriHaximus\Recoil\FiniteCaller;
+use WyriHaximus\Recoil\State;
+
+final class FiniteCallerTest extends TestCase
+{
+    public function testConcurrencyOfOne()
+    {
+        $finished = false;
+        $loop = Factory::create();
+        $kernel = ReactKernel::create($loop);
+        $kernel->setExceptionHandler(function ($error) {
+            echo (string)$error;
+        });
+        $caller = new FiniteCaller($kernel, 1);
+
+        $kernel->execute(function () use ($caller, &$finished) {
+            yield;
+            $deferreds = [];
+            $calls = [];
+            $stream = new Subject();
+            $state = $caller->call($stream);
+            self::assertSame(State::WAITING, $state->getState());
+
+            $deferreds['a'] = new Deferred();
+            $calls['a'] = new  Call(function ($promise) {
+                yield $promise;
+            }, $deferreds['a']->promise());
+            $stream->onNext($calls['a']);
+            self::assertSame(State::BUSY, $state->getState());
+
+            $deferreds['a']->resolve(123);
+            yield new Promise(function ($resolve, $reject) use (&$calls) {
+                $calls['a']->wait($resolve, $reject);
+            });
+            self::assertSame(State::WAITING, $state->getState());
+
+            $deferreds['b'] = new Deferred();
+            $calls['b'] = new  Call(function ($promise) {
+                yield $promise;
+            }, $deferreds['b']->promise());
+            $stream->onNext($calls['b']);
+            self::assertSame(State::BUSY, $state->getState());
+
+            $deferreds['c'] = new Deferred();
+            $calls['c'] = new  Call(function ($promise) {
+                yield $promise;
+            }, $deferreds['c']->promise());
+            $stream->onNext($calls['c']);
+            self::assertSame(State::BUSY, $state->getState());
+
+            $deferreds['b']->resolve(123);
+            yield new Promise(function ($resolve, $reject) use (&$calls) {
+                $calls['b']->wait($resolve, $reject);
+            });
+            self::assertSame(State::BUSY, $state->getState());
+
+            $deferreds['c']->resolve(123);
+            yield new Promise(function ($resolve, $reject) use (&$calls) {
+                $calls['c']->wait($resolve, $reject);
+            });
+            self::assertSame(State::WAITING, $state->getState());
+
+            $finished = true;
+        });
+
+        $loop->run();
+
+        self::assertTrue($finished);
+    }
+
+    public function testConcurrencyOfFive()
+    {
+        $finished = false;
+        $loop = Factory::create();
+        $kernel = ReactKernel::create($loop);
+        $kernel->setExceptionHandler(function ($error) {
+            echo (string)$error;
+        });
+        $caller = new FiniteCaller($kernel, 5);
+
+        $kernel->execute(function () use ($caller, &$finished, $loop) {
+            yield;
+            $deferreds = [];
+            $calls = [];
+            $stream = new Subject();
+            $state = $caller->call($stream);
+            self::assertSame(State::WAITING, $state->getState());
+
+            $deferreds['a'] = new Deferred();
+            $calls['a'] = new  Call(function ($promise) {
+                yield $promise;
+            }, $deferreds['a']->promise());
+            $stream->onNext($calls['a']);
+            self::assertSame(State::WAITING, $state->getState());
+
+            $deferreds['a']->resolve(123);
+            yield new Promise(function ($resolve, $reject) use (&$calls) {
+                $calls['a']->wait($resolve, $reject);
+            });
+            self::assertSame(State::WAITING, $state->getState());
+
+            foreach (['b', 'c', 'd', 'e'] as $i) {
+                $deferreds[$i] = new Deferred();
+                $calls[$i] = new  Call(function ($promise) {
+                    yield $promise;
+                }, $deferreds[$i]->promise());
+                $stream->onNext($calls[$i]);
+                self::assertSame(State::WAITING, $state->getState(), $i);
+            }
+
+            $deferreds['f'] = new Deferred();
+            $stream->onNext(new  Call(function ($promise) {
+                yield $promise;
+            }, $deferreds['f']->promise()));
+            self::assertSame(State::BUSY, $state->getState());
+
+            $deferreds['b']->resolve(123);
+            yield new Promise(function ($resolve, $reject) use (&$calls) {
+                $calls['b']->wait($resolve, $reject);
+            });
+            self::assertSame(State::WAITING, $state->getState());
+
+            $finished = true;
+        });
+
+        $loop->run();
+
+        self::assertTrue($finished);
+    }
+}
