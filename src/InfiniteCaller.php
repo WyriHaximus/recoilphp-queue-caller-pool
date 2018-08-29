362ca99fe643538f998a1bdb5d8f4d61d647030a
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-08-29 17:30:22 +0200

    InfiniteCaller

---
+++ src/InfiniteCaller.php
@@ -0,0 +1,82 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\Recoil;
+
+use Recoil\Kernel;
+use Rx\DisposableInterface;
+use Rx\ObservableInterface;
+use Rx\Subject\Subject;
+
+final class InfiniteCaller implements QueueCallerInterface
+{
+    /** @var Kernel */
+    private $kernel;
+
+    /** @var State */
+    private $state;
+
+    /** @var QueueCallerInterface[] */
+    private $callers = [];
+
+    /** @var string[] */
+    private $readyCallers = [];
+
+    /** @var State[] */
+    private $callerState = [];
+
+    /** @var Subject */
+    private $callerStream = [];
+
+    /**
+     * @param Kernel $kernel
+     */
+    public function __construct(Kernel $kernel)
+    {
+        $this->kernel = $kernel;
+
+        $this->state = new State();
+        $this->state->onNext(State::WAITING);
+    }
+
+    public function call(ObservableInterface $observable): State
+    {
+        try {
+            $observable->subscribe(function (Call $call) {
+                try {
+                    $this->delegateCall($call);
+                } catch (\Throwable $et) {
+                    echo (string)$et;
+                }
+            });
+        } catch (\Throwable $et) {
+            echo (string)$et;
+        }
+
+        return $this->state;
+    }
+
+    private function delegateCall(Call $call): void
+    {
+        /*if (count($this->readyCallers) > 0) {
+            $qcHash = array_pop($this->readyCallers);
+            $this->callerStream[$qcHash]->onNext($call);
+
+            return;
+        }*/
+
+        $stream = new Subject();
+        $caller = new QueueCaller($this->kernel);
+        $qcHash = spl_object_hash($caller);
+
+        $this->callers[$qcHash] = $caller;
+        $this->callerStream[$qcHash] = $stream;
+        $this->callerState[$qcHash] = $caller->call($stream);
+        /** @var DisposableInterface $disposeable */
+        $disposeable = $this->callerState[$qcHash]->filter(function (int $state) {
+            return $state === State::WAITING;
+        })->subscribe(function () use (&$disposeable, $stream, $call) {
+            $stream->onNext($call);
+            $disposeable->dispose();
+        });
+    }
+}
---
+++ tests/InfiniteCallerTest.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\Tests\Recoil;
+
+use ApiClients\Tools\TestUtilities\TestCase;
+use React\EventLoop\Factory;
+use React\Promise\Deferred;
+use React\Promise\Promise;
+use Recoil\React\ReactKernel;
+use Rx\Subject\Subject;
+use WyriHaximus\Recoil\Call;
+use WyriHaximus\Recoil\InfiniteCaller;
+use WyriHaximus\Recoil\State;
+
+final class InfiniteCallerTest extends TestCase
+{
+    public function testInfiniteConcurrency(): void
+    {
+        $finished = false;
+        $loop = Factory::create();
+        $kernel = ReactKernel::create($loop);
+        $kernel->setExceptionHandler(function ($error) {
+            echo (string)$error;
+        });
+        $caller = new InfiniteCaller($kernel);
+
+        $deferreds = [];
+        $kernel->execute(function () use ($caller, &$finished, &$deferreds, $loop) {
+            $calls = [];
+            $stream = new Subject();
+            $state = $caller->call($stream);
+            self::assertSame(State::WAITING, $state->getState());
+
+            for ($i = 'a'; $i !== 'xxx'; $i++) {
+                $deferreds[$i] = new Deferred();
+                $calls[$i] = new Call(function ($promise) {
+                    yield $promise;
+                }, $deferreds[$i]->promise());
+                $stream->onNext($calls[$i]);
+                self::assertSame(State::WAITING, $state->getState(), $i);
+            }
+
+            foreach ($deferreds as $i => $_) {
+                $call = $calls[$i];
+                $deferred = $deferreds[$i];
+                $deferred->resolve(123);
+                yield new Promise(function ($resolve, $reject) use (&$call) {
+                    $call->wait($resolve, $reject);
+                });
+                unset($deferreds[$i]);
+                self::assertSame(State::WAITING, $state->getState(), (string)$i);
+            }
+
+            $finished = true;
+        });
+
+        $loop->run();
+
+        self::assertTrue($finished);
+    }
+}
